42db259f046f943204927c6f29c519eee5d45351
Flavio Pinheiro <flavio.aphn@gmail.com>
2024-02-01 20:01:16 +0000

    Adicionado nova rota e novo html

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
+@app.route('/contato')
 
 def index():
-    return "Olá povo massa!"
+    nome = "Fulano de Tals"
+    dados = {"framework": "Flask", "linguagem": "Python"}
+    return render_template('index.html', nome=nome, dados=dados)
+
+def contato():
+    return render_template('contato.html')
