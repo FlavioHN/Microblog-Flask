de0b07a826e0341a24cdea3ac4b3f061a6408d9c
Flavio Pinheiro <flavio.aphn@gmail.com>
2024-02-02 17:46:10 +0000

    Adicionado encaminhamento entre paginas html

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,11 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-@app.route('/contato')
-
 def index():
     nome = "Fulano de Tals"
     dados = {"framework": "Flask", "linguagem": "Python"}
     return render_template('index.html', nome=nome, dados=dados)
 
+@app.route('/contato')
 def contato():
     return render_template('contato.html')
